f77f4d071b97449ca04ee880d497f6fe1f86723c
Rafindita <rafinditasumar@gmail.com>
2024-03-04 14:30:06 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Fucking World");
+        System.out.println("Hello Damn World");
     }
 }
