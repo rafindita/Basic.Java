35a9351769e053881afbc3792f3b4ed8e69c2f1b
Rafindita <rafinditasumar@gmail.com>
2024-03-04 14:41:34 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Bright Damn World");
+        System.out.println("Hello Bright Shine World");
     }
 }
