2e4d66ead31f2dcd758042d5a10cc745d6471440
Rafindita <rafinditasumar@gmail.com>
2024-03-07 01:50:57 +0700

    ubah nama

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Bright Shine World");
-        System.out.println("I Am Rafindita");
+        System.out.println("I Am Rafi");
         for (int i = 5; i >= 1 ; i--)
             System.out.println(i);
     }
